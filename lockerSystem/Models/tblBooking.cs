e0174eac0dfdee8d77b0551afa8d4c6c85d65f02
Shatham15 <shada1422@hotmail.com>
2024-07-20 20:37:32 +0300

    AddFirst

---
+++ lockerSystem/Models/tblBooking.cs
@@ -0,0 +1,22 @@
+﻿namespace lockerSystem.Models
+{
+    public class tblBooking
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public bool IsDeleted { get; set; }
+        public DateTime  bokingDateTime { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public int phone { get; set; }
+        public tblBookingState BookingState { get; set; }
+        public int BookingStateId { get; set; }
+        public tblLocker Locker { get; set; }
+        public int LockerId { get; set; }
+        public tblSemster Semster { get; set; }
+        public int SemsterId { get; set; }
+        public string? rejectionReason { get; set; } 
+
+        
+    }
+}
---
+++ lockerSystem/Models/tblBookingState.cs
@@ -0,0 +1,12 @@
+﻿namespace lockerSystem.Models
+{
+    public class tblBookingState
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public bool IsDeleted { get; set; }
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public ICollection<tblBooking> Bookings { get; set; }
+    }
+}
