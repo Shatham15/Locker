caf77184bfd3a39f0e4042e526f569458e1c7893
Gufran2000 <ghfranalmtr@gmail.com>
2024-07-28 14:31:14 +0300

    iikk

--- lockerSystem/Controllers/UserController.cs
+++ lockerSystem/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using lockerSystem.Domain;
 using lockerSystem.Models;
+using lockerSystem.ViewsModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lockerSystem.Controllers
@@ -11,9 +12,9 @@ namespace lockerSystem.Controllers
 
             _userDomain = userDomain;
         }
-        public IActionResult Index()
+        public async Task<  IActionResult > Index()
         {
-            return View(_userDomain.GetAllUsers());
+            return View( await _userDomain.GetAllUsers());
         }
         [HttpGet]
         public IActionResult add()
@@ -21,9 +22,23 @@ namespace lockerSystem.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult add(tblUser user)
+        public IActionResult add(UserViewsModels user)
         {
-            _userDomain.addUser(user);  
+            if(ModelState.IsValid)
+            {
+                _userDomain.addUser(user);
+            }
+            return View(user);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(_userDomain.getUserById(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(tblUser user)
+        {
+            _userDomain.editUser(user);
             return View(user);
         }
 
--- lockerSystem/Domain/UserDomain.cs
+++ lockerSystem/Domain/UserDomain.cs
@@ -1,4 +1,6 @@
 ﻿using lockerSystem.Models;
+using lockerSystem.ViewsModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace lockerSystem.Domain
 {
@@ -9,14 +11,43 @@ namespace lockerSystem.Domain
         {
             _context = context;
         }
-        public IEnumerable<tblUser> GetAllUsers() {
-            return _context.tblUser;// select * from tblUser
+        public async Task<IEnumerable<UserViewsModels>>   GetAllUsers() {
+
+            return await  _context.tblUser.Select(x=> new UserViewsModels
+            {
+                Id = x.Id,
+                email = x.email, 
+                fullName = x.fullName,
+                password = x.password,
+                phone = x.phone,
+                userType = x.userType
+            }).ToListAsync();// select * from tblUser
         }
-        public string addUser(tblUser user)
+        public string addUser(UserViewsModels user)
         {
-            _context.Add(user);
+            tblUser userinfo=new tblUser();
+            userinfo.fullName = user.fullName;
+            userinfo.email = user.email;
+            userinfo.password = user.password;
+            userinfo.phone = user.phone;
+            userinfo.userType = user.
+        userType;
+                
+
+
+            _context.Add(userinfo);
             _context.SaveChanges();
-            return "addede";      
+            return "add";      
+        }
+        public tblUser getUserById(int id)
+        {
+            return _context.tblUser.FirstOrDefault(x => x.Id== id);
+        }
+        public string editUser(tblUser user)
+        {
+            _context.Update(user);
+            _context.SaveChanges();
+            return "edit";
         }
     }
 }
