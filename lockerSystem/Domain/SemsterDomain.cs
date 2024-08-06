eaa652b0362521917e8bb73e430f28ea0cca87db
hp <hp@DESKTOP-DI54SRE>
2024-08-06 12:46:50 +0300

    'Commit Message'

---
+++ lockerSystem/Domain/SemsterDomain.cs
@@ -0,0 +1,124 @@
+﻿using lockerSystem.Models;
+using lockerSystem.ViewsModels;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace lockerSystem.Domain
+{
+    public class SemsterDomain
+    {
+        private readonly LockerSystemContext _context;
+        public SemsterDomain(LockerSystemContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<SemsterViewsModels>> GetAllSemsters()
+        {
+
+            return await _context.tblSemster.Where(s => s.IsActive == false).Select(x => new SemsterViewsModels
+            {
+
+                //Id = x.Id,
+                Guid = x.Guid,
+                startSemster = x.startSemster,
+                endSemster = x.endSemster,
+                semsterNameAr = x.semsterNameAr,
+                semsterNameEn = x.semsterNameEn,
+
+
+
+            }).ToListAsync();// select * from tblBuilding
+        }
+        public string addSemster(SemsterViewsModels Semster)
+        {
+
+            try
+            {
+                tblSemster Semsterinfo = new tblSemster();
+                Semsterinfo.Guid = Semster.Guid;
+                Semsterinfo.startSemster = Semster.startSemster;
+                Semsterinfo.endSemster = Semster.endSemster;
+                Semsterinfo.semsterNameAr = Semster.semsterNameAr;
+                Semsterinfo.semsterNameEn = Semster.semsterNameEn;
+
+                _context.Add(Semsterinfo);
+                _context.SaveChanges();
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return "-1";
+            }
+        }
+        public SemsterViewsModels getSemsterById(Guid id)
+        {
+            var Semsterinfo = _context.tblSemster.FirstOrDefault(x => x.Guid == id);
+            SemsterViewsModels models = new SemsterViewsModels
+            {
+
+                Guid = Semsterinfo.Guid,
+                startSemster = Semsterinfo.startSemster,
+                endSemster = Semsterinfo.endSemster,
+                semsterNameAr = Semsterinfo.semsterNameAr,
+                semsterNameEn = Semsterinfo.semsterNameEn,
+
+            }; return models;
+
+
+        }
+        public tblSemster getSemsterByGuid(Guid id)
+        {
+            return _context.tblSemster.FirstOrDefault(x => x.Guid == id);
+
+
+
+
+        }
+        public string editSemster(SemsterViewsModels Semster)
+        {
+
+
+            try
+            {
+                tblSemster Semsterinfo = getSemsterByGuid(Semster.Guid);
+                Semsterinfo.startSemster = Semster.startSemster;
+                Semsterinfo.endSemster = Semster.endSemster;
+                Semsterinfo.semsterNameAr = Semster.semsterNameAr;
+                Semsterinfo.semsterNameEn = Semster.semsterNameEn;
+
+                _context.Update(Semsterinfo);
+                _context.SaveChanges();
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return " فشل التعديل!!";
+            }
+
+        }
+
+
+
+        public string DeleteSemster(Guid id/*, bool permanentDelete = false*/)
+        {
+            try
+            {
+                var semsterInfo = getSemsterByGuid(id);
+                semsterInfo.IsActive = true;
+                _context.Update(semsterInfo);
+                _context.SaveChanges();
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return "تم الحذف بنجاح";
+
+            }
+
+
+        }
+
+
+
+    }
+}
