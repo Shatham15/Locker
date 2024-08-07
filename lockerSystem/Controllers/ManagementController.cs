9f59011387dc738b82bf4cdfc40038b420b01f32
Shatham15 <shada1422@hotmail.com>
2024-08-07 13:14:51 +0300

    Management

---
+++ lockerSystem/Controllers/ManagementController.cs
@@ -0,0 +1,103 @@
+﻿using lockerSystem.Domain;
+using lockerSystem.Models;
+using lockerSystem.ViewsModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lockerSystem.Controllers
+{
+    public class ManagementController : Controller
+    {
+        private readonly ManagementDomain _managementdomain;
+
+        public ManagementController(ManagementDomain managementdomain)
+        {
+            _managementdomain = managementdomain;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _managementdomain.GetAllMangement());
+        }
+        [HttpGet]
+        public IActionResult add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult add(ManagementViewsModels management)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                string check = _managementdomain.addMangement(management);
+                if (check == "1")
+
+                    ViewData["Successful"] = "تمت الاضافة بنجاح";
+
+                else
+
+                    ViewData["Falied"] = check;
+            }
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult edit(Guid id)
+        {
+
+            return View(_managementdomain.getManagementById(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult edit(ManagementViewsModels management)
+        {
+
+
+
+            if (ModelState.IsValid)
+            {
+                string check = _managementdomain.editManagement(management);
+                if (check == "1")
+
+                    ViewData["Successful"] = "تم التعديل بنجاح";
+
+                else
+
+                    ViewData["Falied"] = check;
+            }
+            return View();
+
+
+
+        }
+
+
+
+        public IActionResult Delete(Guid id)
+        {
+
+            //_managementdomain.deleteManagement(id);
+            string check = _managementdomain.DeleteManagement(id);
+            if (check == "1")
+
+                ViewData["Successful"] = "تم الحذف بنجاح";
+
+            else
+
+                ViewData["Falied"] = check;
+
+            _managementdomain.DeleteManagement(id);
+            return View();
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ lockerSystem/Domain/ManagementDomain.cs
@@ -0,0 +1,119 @@
+﻿using lockerSystem.Domain;
+using lockerSystem.Models;
+using lockerSystem.ViewsModels;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
+
+namespace lockerSystem.Domain
+{
+    public class ManagementDomain
+    {
+        private readonly LockerSystemContext _managementContext;
+
+        public ManagementDomain(LockerSystemContext managementcontext)
+        {
+            _managementContext = managementcontext;
+        }
+
+        public async Task<IEnumerable<ManagementViewsModels>> GetAllMangement() //مافهمت فكرةTask 
+
+        {
+            return await _managementContext.tblManagement.Where(m => m.IsDeleted == false).Select(x => new ManagementViewsModels
+            {
+                // Id = x.Id,
+                Guid = x.Guid,
+                name = x.name,
+                value = x.value
+            }).ToListAsync();// I do not understand what the ToList??
+
+        }
+
+        public string addMangement(ManagementViewsModels management)
+        {
+            try
+            {
+                tblManagement managementInfo = new tblManagement();
+                managementInfo.name = management.name;
+                managementInfo.value = management.value;
+
+
+                _managementContext.tblManagement.Add(managementInfo);
+                _managementContext.SaveChanges();
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return "فشلت عملية الاضافة !!";
+            }
+
+
+        }
+        public ManagementViewsModels getManagementById(Guid? id)
+        {
+            var managementInfo = _managementContext.tblManagement.FirstOrDefault(x => x.Guid == id);
+            ManagementViewsModels models = new ManagementViewsModels
+            {
+                //Id = managementInfo.Id,
+                Guid = managementInfo.Guid,
+                name = managementInfo.name,
+                value = managementInfo.value
+            };
+            return models;
+
+        }
+
+        public tblManagement getManagementByGuid(Guid? id)
+        {
+
+
+            return _managementContext.tblManagement.FirstOrDefault(x => x.Guid == id);
+
+
+
+        }
+
+        public string editManagement(ManagementViewsModels management)
+        {
+            try
+            {
+                tblManagement managementInfo = getManagementByGuid(management.Guid);
+
+                managementInfo.name = management.name;
+                managementInfo.value = management.value;
+                //management.IsDeleted = false;
+
+                _managementContext.Update(managementInfo);
+                _managementContext.SaveChanges();
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return "فشلت عملية التعديل ";
+            }
+
+        }
+
+
+        public string DeleteManagement(Guid id)
+
+        {
+            try
+            {
+                tblManagement managementInfo = getManagementByGuid(id);
+                managementInfo.IsDeleted = true;
+                _managementContext.Update(managementInfo);
+                _managementContext.SaveChanges();
+                return "1";
+            }
+            catch (Exception ex)
+            {
+                return " فشلت عملية الحذف!!";
+            }
+
+
+        }
+
+
+    }
+}
+
