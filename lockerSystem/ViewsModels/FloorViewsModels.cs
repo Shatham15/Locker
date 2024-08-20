c20c5b8fe3999ff94828f248b16d4f01641e6117
Shatham15 <shada1422@hotmail.com>
2024-08-20 17:18:53 +0300

    floor and lockerstate

--- lockerSystem/ViewsModels/FloorViewsModels.cs
+++ lockerSystem/ViewsModels/FloorViewsModels.cs
@@ -6,16 +6,18 @@ namespace lockerSystem.ViewsModels
 {
     public class FloorViewsModels
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public Guid Guid { get; set; } = Guid.NewGuid();
         public bool IsDeleted { get; set; }
         [Required(ErrorMessage = "هذا الحقل مطلوب")]
+        [Range(1, 10, ErrorMessage = "يجب ان يكون رقم المبنى من 1 الى 5")]
         [DisplayName("رقم الطابق")]
         public int no { get; set; }
         public tblBuilding Building { get; set; }
         [Required(ErrorMessage = "هذا الحقل مطلوب")]
         [DisplayName("اسم المبنى")]
         public int BuildingId { get; set; }
+
         [DisplayName("اسم المبنى")]
         public string BuildingName { get; set; }
         public ICollection<tblLocker> Lockers { get; }
